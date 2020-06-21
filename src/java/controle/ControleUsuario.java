129f67a229acbc667dd088721af4232efa83f926
carlosdev@outlook.com.br <carlosdev@outlook.com.br>
2020-06-20 22:50:00 -0300

    Inserido sistema de Login

--- src/java/controle/ControleUsuario.java
+++ src/java/controle/ControleUsuario.java
@@ -1,28 +1,65 @@
 
 package controle;
 
-import static java.lang.Integer.parseInt;
-import java.sql.SQLException;
-import java.text.DecimalFormat;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.ArrayList;
-import objeto.Teste;
+
 import objeto.Usuario;
+import web.DbListener;
 
 public class ControleUsuario {
 
-    public static Usuario login(Usuario usuario){
-        
-        return usuario;
-    }
-    
-    public static String getMediaUsuario(String codigoUsuario) throws ClassNotFoundException, SQLException{
-        ArrayList<Teste> testesRealizados = ControleQuiz.getUltimosTestesRealizadosUsuario(codigoUsuario);
-        double somaTotalAcertos = 0;
-        DecimalFormat df = new DecimalFormat("#.00");
-        
-        for(Teste teste: testesRealizados){
-            somaTotalAcertos = somaTotalAcertos + parseInt(teste.getResultado());
-        }
-        return df.format(somaTotalAcertos/testesRealizados.size());
+	public static ArrayList<Usuario> login(Usuario usuario) throws Exception {
+
+		ArrayList<Usuario> list = new ArrayList<>();
+		Class.forName("org.sqlite.JDBC");
+		Connection con = DriverManager.getConnection(DbListener.JDBCURL);
+		Statement stmt = con.createStatement();
+		ResultSet rs = stmt.executeQuery("SELECT * FROM usuario");
+		while (rs.next()) {
+			list.add(new Usuario(rs.getString("nome"), rs.getString("login"), rs.getString("senha")));
+		}
+		rs.close();
+		stmt.close();
+		con.close();
+		return list;
+	}
+
+	public static Usuario getUsuario(String login, String senha) throws Exception {
+		Usuario usuario = null;
+		Class.forName("org.sqlite.JDBC");
+		Connection con = DriverManager.getConnection(DbListener.JDBCURL);
+		String SQL = "SELECT * FROM usuario WHERE nm_login_usuario=? AND cd_senha_hash=?";
+		PreparedStatement stmt = con.prepareStatement(SQL);
+		stmt.setString(1, login);
+		stmt.setLong(2, senha.hashCode());
+		ResultSet rs = stmt.executeQuery();
+		if (rs.next()) {
+			usuario.setLogin(rs.getString("login"));
+			usuario.setSenha(rs.getString("senha"));
+		} else {
+
+		}
+		rs.close();
+		stmt.close();
+		con.close();
+		return usuario;
+	}
+	
+	public static void setUsuario(String nome, String login, String senha) throws Exception{
+        Class.forName("org.sqlite.JDBC");
+        Connection con = DriverManager.getConnection(DbListener.JDBCURL);
+        String SQL = "INSERT INTO usuarios(nm_usuario, nm_login_usuario, cd_senha_hash) VALUES(?,?,?)";
+        PreparedStatement stmt = con.prepareStatement(SQL);
+        stmt.setString(1, nome);
+        stmt.setString(2, login);
+        stmt.setLong(3, senha.hashCode());
+        stmt.execute();
+        stmt.close();
+        con.close();
     }
 }
--- src/java/objeto/Usuario.java
+++ src/java/objeto/Usuario.java
@@ -1,44 +1,49 @@
 
 package objeto;
 
-
 public class Usuario {
-   
-    private String codigoUsuario;
-    private String nome;
-    private String login;
-    private String senha;
-    
-    public String getCodigoUsuario() {
-        return codigoUsuario;
-    }
 
-    public void setCodigoUsuario(String codigoUsuario) {
-        this.codigoUsuario = codigoUsuario;
-    }
+	private String codigoUsuario;
+	private String nome;
+	private String login;
+	private String senha;
 
-    public String getNome() {
-        return nome;
-    }
+	public Usuario(String nome, String login, String senha) {
+		this.nome = nome;
+		this.login = login;
+		this.senha = senha;
+	}
+	
+	public String getCodigoUsuario() {
+		return codigoUsuario;
+	}
 
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
+	public void setCodigoUsuario(String codigoUsuario) {
+		this.codigoUsuario = codigoUsuario;
+	}
 
-    public String getLogin() {
-        return login;
-    }
+	public String getNome() {
+		return nome;
+	}
 
-    public void setLogin(String login) {
-        this.login = login;
-    }
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
 
-    public String getSenha() {
-        return senha;
-    }
+	public String getLogin() {
+		return login;
+	}
 
-    public void setSenha(String senha) {
-        this.senha = senha;
-    }
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
 
 }
